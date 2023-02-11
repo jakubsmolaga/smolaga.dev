04d39caa2c28f6016be811eb8598c730488817c9
Jakub Smolaga <01159153@pw.edu.pl>
2023-02-11 20:40:40 +0100

    added readme

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Personal Website
+This is a simple website that contains all of my links
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 import { UserConfig } from "vite";
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import viteCompression from 'vite-plugin-compression';
-// import htmlPurge from 'vite-plugin-html-purgecss';
 import htmlPurge from 'vite-plugin-purgecss';
 
 export default (): UserConfig => {
     return {
         plugins: [
-            // htmlPurge() as any,
             htmlPurge({}) as any,
             viteCompression({
                 algorithm: 'brotliCompress',
                 filter: /\.(js|mjs|json|css|html|glb)$/i,
-                // deleteOriginFile: true,
+                deleteOriginFile: true,
             }),
             ViteMinifyPlugin({})
         ],
