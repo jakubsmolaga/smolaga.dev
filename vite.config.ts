0c4573ac09c0e4849d537abd59578b04b7a33a91
Jakub Smolaga <01159153@pw.edu.pl>
2023-02-11 21:26:58 +0100

    remove precompression

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { UserConfig } from "vite";
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
-import viteCompression from 'vite-plugin-compression';
+// import viteCompression from 'vite-plugin-compression';
 import htmlPurge from 'vite-plugin-purgecss';
 
 export default (): UserConfig => {
     return {
         plugins: [
             htmlPurge({}) as any,
-            viteCompression({
-                algorithm: 'brotliCompress',
-                filter: /\.(js|mjs|json|css|html|glb)$/i,
-                deleteOriginFile: true,
-            }),
+            // viteCompression({
+            //     algorithm: 'brotliCompress',
+            //     filter: /\.(js|mjs|json|css|html|glb)$/i,
+            //     deleteOriginFile: true,
+            // }),
             ViteMinifyPlugin({})
         ],
     };
