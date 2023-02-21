8688bca22eccbbdd9c5ed0e6276663ca186f7f45
Jakub Smolaga <01159153@pw.edu.pl>
2023-02-21 23:31:43 +0100

    added cssnano and vite-plugin-minify

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { UserConfig } from "vite";
-// import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import cssnano from 'cssnano';
 import postcssJitProps from 'postcss-jit-props';
 import OpenProps from 'open-props';
 
 export default (): UserConfig => {
     return {
         base: "./",
-        plugins: [],
+        plugins: [
+            ViteMinifyPlugin(),
+        ],
         css: {
             postcss: {
                 plugins: [
                     postcssJitProps(OpenProps),
+                    cssnano(),
                 ],
             }
         }
